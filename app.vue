3c276ac7619e7aa1464efa7b6dbc7948fe3fbcfd
rizalrenaldi <rizalrenaldi@gmail.com>
2024-10-25 13:51:53 +0700

    styling

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>
+    <div class="item">
       <div>NuxtImg tag</div>
       <NuxtImg 
       src="/1.jpg" 
@@ -8,22 +8,26 @@
       width="900"
       />
     </div>
-    <div>
+    <div class="item">
       <div>icon.png NuxtImg tag with preset</div>
       <NuxtImg 
       src="/icon.png" 
       alt="icon" 
+      width="40"
+      height="40"
       preset="icon"
       />
     </div>
-    <div>
+    <div class="item">
       <div>icon.png img tag</div>
       <img 
       src="/icon.png" 
       alt="icon"
+      width="40"
+      height="40"
       />
     </div>
-    <div>
+    <div class="item">
       <div>component with NuxtImg in it</div>
       <TheImage 
       src="/1.jpg" 
@@ -31,7 +35,7 @@
       width="900"
     />
     </div>
-    <div>
+    <div class="item">
       <div>img tag</div>
       <img 
       src="/1.jpg" 
@@ -52,3 +56,10 @@ const imgs = [
   '5.png'
 ]
 </script>
+
+<style>
+.item {
+  padding: 24px 0px;
+  border-bottom: 1px solid #333;
+}
+</style>
